06b7847030ce1ee7cebfee8b791f24edfdcf1d16
Murilo_Silva <Murilo.vfs.472@gmail.com>
2026-05-18 19:52:26 -0300

    Adicionando validação de adm via api

--- cypress/e2e/cadastro.cy.js
+++ cypress/e2e/cadastro.cy.js
@@ -1,30 +1,28 @@
 describe('Cadastro de usuário', () => {
     it('Cadastro de usuário regular com sucesso', () => {
-        cy.intercept('POST', '**/usuarios').as('Cadastro_Usuario')
         cy.visit('/cadastrarusuarios')
         cy.get('[data-testid="nome"]').type('Murilo Vinicius Silva')
         cy.get('[data-testid="email"]').type('murilo@gmail.com.br')
+        cy.get('[data-testid="password"]').type('teste123')
+        cy.get('[data-testid="checkbox"]').uncheck()
+        cy.get('[data-testid="cadastrar"]').click()
+
+        cy.contains('a', 'Cadastro realizado com sucesso', {timeout: 6000}).should('be.visible')
+        cy.ehAdm('Murilo Vinicius Silva' , 'murilo@gmail.com.br', 'nao')
+    })
+
+    it('Cadastro de usuário adm com sucesso', () => {
+        cy.intercept('POST', '**/usuarios').as('Cadastro_Usuario')
+        cy.visit('/cadastrarusuarios')
+        cy.get('[data-testid="nome"]').type('Murilo Vinicius Silva')
+        cy.get('[data-testid="email"]').type('murilo123@gmail.com.br')
         cy.get('[data-testid="password"]').type('teste123')
         cy.get('[data-testid="checkbox"]').check()
         cy.get('[data-testid="cadastrar"]').click()
 
         cy.wait('@Cadastro_Usuario').its('response.statusCode').should('eq', 201)
         cy.contains('a', 'Cadastro realizado com sucesso').should('be.visible')
-        cy.ehAdm('sim')
-    })
-
-    it('Cadastro de usuário regular com sucesso', () => {
-        cy.intercept('POST', '**/usuarios').as('Cadastro_Usuario')
-        cy.visit('/cadastrarusuarios')
-        cy.get('[data-testid="nome"]').type('Murilo Vinicius Silva')
-        cy.get('[data-testid="email"]').type('murilo123@gmail.com.br')
-        cy.get('[data-testid="password"]').type('teste123')
-        cy.get('[data-testid="checkbox"]').uncheck()
-        cy.get('[data-testid="cadastrar"]').click()
-
-        cy.wait('@Cadastro_Usuario').its('response.statusCode').should('eq', 201)
-        cy.contains('a', 'Cadastro realizado com sucesso').should('be.visible')
-        cy.ehAdm('nao')
+        cy.ehAdm('Murilo Vinicius Silva', 'murilo123@gmail.com.br', 'sim')
     })
 
     it('Cadastro sem sucesso - sem credenciais fornecidas', () => {
--- cypress/support/api_commands.js
+++ cypress/support/api_commands.js
@@ -1,6 +1,19 @@
-// Cypress.Commands.add('Login', (email, password, pagina) => {
-//     cy.visit(pagina)
-//     cy.get('#user-name').type(email)
-//     cy.get('#password').type(password)
-//     cy.get('#login-button').click()
-// })
+Cypress.Commands.add('ehAdm', (usuario, email, adm) => {
+    cy.request({
+        method: 'GET',
+        url: 'https://serverest.dev/usuarios',
+        qs: {
+            nome: usuario,
+            email: email
+        }
+    }).then((response) => {
+        expect(response.status).to.eq(200)
+        var administrador_response = response.body.usuarios[0].administrador
+
+        if (adm === 'sim') {
+            expect(administrador_response).to.be.equal('true')
+        } else {
+            expect(administrador_response).to.be.equal('false')
+        }
+    })
+})
