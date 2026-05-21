2faa6ef7c9bce458b05ebbe7e5334d60abb79460
murilo-silva-ai <murilo.vfs.472@gmail.com>
2026-05-21 20:27:23 -0300

    Mock e apis

--- cypress/support/api_commands.js
+++ cypress/support/api_commands.js
@@ -17,3 +17,45 @@ Cypress.Commands.add('ehAdm', (usuario, email, adm) => {
         }
     })
 })
+
+Cypress.Commands.add('cadastraUsuario', (nome, email, senha, administrador) => {
+    cy.request({
+        method: 'POST',
+        url: 'https://serverest.dev/usuarios',
+        body: {
+            nome: nome,
+            email: email,
+            password: senha,
+            administrador: administrador
+        }
+    })
+})
+
+Cypress.Commands.add('deleteUsuario', (email) => {
+    cy.request({
+        method: 'GET',
+        url: 'https://serverest.dev/usuarios',
+        qs: {
+            email: email
+        }
+    }).then((response) => {
+        cy.request({
+            method: 'DELETE',
+            url: `https://serverest.dev/usuarios/${response.body.usuarios[0]._id}`,
+        })
+    })
+})
+
+Cypress.Commands.add('login_api', (email, senha, tela) => {
+    cy.request({
+        method: 'POST',
+        url: 'https://serverest.dev/login',
+        body: {
+            email: email,
+            password: senha
+        }
+    }).then((response) => {
+        window.localStorage.setItem('serverest/userToken', response.body.authorization)
+    })
+    cy.visit(`/${tela}`)
+})
